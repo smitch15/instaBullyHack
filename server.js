21b5ecf691cc556faf6182c56f978a4b6d85d783
Steven Mitchell <smitch15@binghamton.edu>
2017-02-27 21:02:54 -0500

    update server.js

--- server.js
+++ server.js
@@ -7,9 +7,30 @@ var cheerio = require('cheerio');
 var app     = express();
 
 app.get('/scrape', function(req, res){
+    
+    // The URL we will scrape from - in our example Anchorman 2.
+    url = 'http://www.imdb.com/title/tt1229340/';
 
-  //All the web scraping magic will happen here
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+	// callback function gets called if the request fails
+	// you make request with url and a callback function that takes an error, a response, and html
+    request(url, function(error, response, html){
 
+        // First we'll check to make sure no errors occurred when making the request
+        if(!error){
+            
+			// Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+            // cheerio is used to make simple jQueries in node js
+			var $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+            var title, release, rating;
+			// create json to store the captured information from the cheerio style jquery of the imdb url
+            var json = { title : "", release : "", rating : ""};
+        }
+    })
 })
 
 app.listen('8081')
